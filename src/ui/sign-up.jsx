e1e0ca1832fa69ed293328bcad9cb75ace8e7389
Samuel Fortunato <samfortunato@gmail.com>
2021-05-01 20:01:55 -0400

    Make password validations visible to user on signup page

--- src/ui/sign-up.jsx
+++ src/ui/sign-up.jsx
@@ -18,24 +18,41 @@ export function SignUp() {
   async function handleSignUp(evt) {
     evt.preventDefault();
 
-    setIsSigningUp(true);
+    if (canSignUp()) {
+      setIsSigningUp(true);
 
-    try {
-      await AuthService.signUp(username, email, password);
+      try {
+        await AuthService.signUp(username, email, password);
 
-      const stripe = await stripePromise;
-      const session = await HttpService.jsonRequest('http://ec2-100-25-200-25.compute-1.amazonaws.com:8082/create-checkout-session', { method: 'POST' });
+        const stripe = await stripePromise;
+        const session = await HttpService.jsonRequest('http://ec2-100-25-200-25.compute-1.amazonaws.com:8082/create-checkout-session', { method: 'POST' });
 
-      const result = await stripe.redirectToCheckout({ sessionId: session.id });
-      if (result.error) console.error(result.error.message);
+        const result = await stripe.redirectToCheckout({ sessionId: session.id });
 
-      // history.push('/auth/log-in');
-    } catch (err) {
-      console.error(err);
-    } finally {
-      setIsSigningUp(false);
+        if (result.error) throw result.error;
+
+        // history.push('/auth/log-in');
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setIsSigningUp(false);
+      }
     }
   }
+
+  function canSignUp() {
+    return hasTenCharsOrMore() &&
+      hasOneOrMoreLowercaseLetters() &&
+      hasOneOrMoreUppercaseLetters() &&
+      hasOneOrMoreNumbers() &&
+      hasOneOrMoreSpecialCharacters();
+  }
+
+  function hasTenCharsOrMore() { return password.length >= 10; }
+  function hasOneOrMoreLowercaseLetters() { return /[a-z]/.test(password); }
+  function hasOneOrMoreUppercaseLetters() { return /[A-Z]/.test(password); }
+  function hasOneOrMoreNumbers() { return /\d/.test(password); }
+  function hasOneOrMoreSpecialCharacters() { return /\W/.test(password); }
 
   return (
     <>
@@ -72,9 +89,17 @@ export function SignUp() {
           />
         </label>
 
-        <input type="submit" value="Sign Up, Idiot!" disabled={isSigningUp} />
+        <input type="submit" value="Sign Up, Idiot!" disabled={!canSignUp() || isSigningUp} />
 
         {isSigningUp && <span>Loading...</span>}
+
+        <ul id="password-requirements">
+          <li data-is-valid={hasTenCharsOrMore()}>Password must be 10 characters or more</li>
+          <li data-is-valid={hasOneOrMoreLowercaseLetters()}>Password must have at least 1 lowercase letter</li>
+          <li data-is-valid={hasOneOrMoreUppercaseLetters()}>Password must have at least 1 uppercase letter</li>
+          <li data-is-valid={hasOneOrMoreNumbers()}>Password must have at least 1 number</li>
+          <li data-is-valid={hasOneOrMoreSpecialCharacters()}>Password must have at least 1 special character (!, @, #, etc.)</li>
+        </ul>
 
         <p>
           Once you sign up, you will be directed to the <strong>payment form.</strong>
